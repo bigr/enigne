249b6ac322d56341dd6a2c8ddaae8d30f9dad30d
Pavel Klinger <ja@bigr.cz>
2020-03-20 10:15:25 +0100

    Principal variation for search.

--- enigne/tests/enigne/test_search.py
+++ enigne/tests/enigne/test_search.py
@@ -5,7 +5,7 @@ from enigne.board import Board
 from enigne.search import alphabeta_search
 
 
-@pytest.mark.parametrize('fen, depth, score', [
+@pytest.mark.parametrize('fen, depth, expected_score', [
     ('8/2k5/5p2/4P3/8/4K3/8/8 b - - 0 1', 1, 1),
     ('8/2k5/5p2/4P3/8/4K3/8/8 w - - 0 1', 1, 1),
     ('8/2k5/3n1p2/4P3/8/4K3/8/8 w - - 0 1', 1, 0),
@@ -16,6 +16,16 @@ from enigne.search import alphabeta_search
     ('K7/3N4/8/3p4/3P4/8/6k1/8 w - - 0 1', 2, 3),
     ('K7/3N4/8/3p4/3P4/8/6k1/8 w - - 0 1', 3, 4),
 ])
-def test_alphabeta_search(fen, depth, score):
+def test_alphabeta_search(fen, depth, expected_score):
     board = Board(fen)
-    assert alphabeta_search(board, depth) == score
+    score, pv = alphabeta_search(board, depth)
+    assert score == expected_score
+    assert len(pv) <= depth
+
+    if abs(score) != math.inf and score != 0:
+        assert len(pv) == depth
+
+    for i, move in enumerate(pv, 1):
+        board.move(move)
+        score, _ = alphabeta_search(board, depth - i)
+        assert score == expected_score * (-1 if i % 2 else 1)
