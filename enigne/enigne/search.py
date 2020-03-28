26b909309a3f4af3b45282d4387525c85914c776
Pavel Klinger <ja@bigr.cz>
2020-03-28 08:59:21 +0100

    Search fix

--- enigne/enigne/search.py
+++ enigne/enigne/search.py
@@ -5,26 +5,30 @@ from enigne.board import Board, Move
 from enigne.eval import evaluate_material
 from enigne.move_gen import legal_move_gen, in_check
 
+MATE_SCORE = 32767
+
 
 def alphabeta_search(board: Board, depth: int, alpha: float = -math.inf,
                      beta: float = math.inf) -> Tuple[float, List[Move]]:
+    """Negamax implementation of alpha-beta pruning."""
     if depth == 0:
         return evaluate_material(board), []
 
     pv = []
-    value = -math.inf
     mate = True
+    score_mx = -math.inf
     for move in legal_move_gen(board):
         mate = False
         with board.do_move(move):
-            sub_value, sub_pv = alphabeta_search(board, depth - 1, alpha, beta)
-            if -sub_value > value:
-                value = -sub_value
-                pv = [move] + sub_pv
-            alpha = max(alpha, value)
-            if alpha >= beta:
-                break
-    if mate and not in_check(board):
-        value = 0
+            score, sub_pv = alphabeta_search(board, depth - 1, -beta, -alpha)
+        score *= -1
+        if score >= beta and score != math.inf:
+            return beta, pv
+        if score > alpha:
+            alpha = score
+            pv = [move] + sub_pv
 
-    return value, pv
+    if mate:
+        return (-MATE_SCORE if in_check(board) else 0), []
+
+    return alpha, pv
