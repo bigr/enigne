4959923e5228aefa52785538e2be2da573fef2ea
Pavel Klinger <ja@bigr.cz>
2020-04-17 12:57:40 +0200

    Engine about

--- enigne/setup.py
+++ enigne/setup.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python3
-
+import os
 from setuptools import setup, find_packages
+
+about = {}
+with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "enigne", "__about__.py")) as fp:
+    exec(fp.read(), about)
+
 setup(
-    name="Enigne",
-    version="0.1",
+    name=about['__name__'],
+    version=about['__version__'],
+    author=about['__author__'],
     packages=find_packages(),
     scripts=['bin/enigne-perft'],
     tests_require=['pytest', 'pytest-console-scripts'],
